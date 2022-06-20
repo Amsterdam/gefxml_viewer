7707471a015a4af54164dea8fc75a4355685b174
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2022-06-20 13:36:44 +0200

    test bestanden verwijderd uit git

--- test_sikb.py
+++
@@ -1,8 +0,0 @@
-from gefxml_reader import Bore, Multibore
-
-mb = Multibore()
-mb.load_xml_sikb0101('./input/2010P258.xml')
-mb.load_xml_sikb0101('./input/imsikb_v14-project-229492_verkennendbodemonderzoekkabel-enleidingtraceterpl_20220610084424549.xml')
-
-for bore in mb.bores:
-    bore.plot()
