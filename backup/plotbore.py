c58fa566ee2ffb809964df822e5029d801f06922
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2022-06-20 13:48:01 +0200

    backup verwijderd

--- backup/plotbore.py
+++
@@ -1,9 +0,0 @@
-from gefxml_reader import Bore
-
-f = './input/DP14+074_MB_KR.xml'
-f = './input/bore.xml'
-f = './input/DCG0101_B_BHR000000347910.xml'
-
-bore = Bore()
-bore.load_xml(f)
-bore.plot()
