43d1dd0e4b2d1bf94c4cf7efcfb190e8b04e21fa
Thomas van der Linden <t.van.der.linden@amsterdam.nl>
2023-02-23 17:42:16 +0100

    filetype voor output als optie toegevoegd

--- gefxml_reader.py
+++ gefxml_reader.py
@@ -584,7 +584,7 @@ class Cpt(Test):
                 self.data["frictionRatio"] = 0
 
 
-    def plot(self, path='./output'):
+    def plot(self, path='./output', outputType='png'):
         if self.groundlevel == None:
             self.groundlevel = 0
 
@@ -691,17 +691,17 @@ class Cpt(Test):
 
         # sla de figuur op
         plt.tight_layout()
-        plt.savefig(fname=f"./output/{self.filename}.png")
+        plt.savefig(fname=f"./output/{self.filename}.{outputType}")
         plt.close('all')
 
         # andere optie voor bestandsnaam
         save_as_projectid_fromfile = False
         if save_as_projectid_fromfile:
             if self.projectid is not None: # TODO: dit moet ergens anders. Moet ook projectid uit mapid kunnen halen
-                plt.savefig(fname=f"./output/{self.projectid}_{self.testid}.png")
+                plt.savefig(fname=f"./output/{self.projectid}_{self.testid}.{outputType}")
                 plt.close('all')
             elif self.projectname is not None:
-                plt.savefig(fname=f"{path}/{self.projectname}_{self.testid}.png")
+                plt.savefig(fname=f"{path}/{self.projectname}_{self.testid}.{outputType}")
                 plt.close('all')
 
     def check_add_depth(self):
@@ -1152,7 +1152,7 @@ class Bore(Test):
         self.soillayers['veld']["components"] = components
         return self.soillayers
 
-    def plot(self, path='./output'):
+    def plot(self, path='./output', outputType='png'):
 
         materials = {0: 'grind', 1: 'zand', 2: 'klei', 3: 'leem', 4: 'veen', 5: 'silt', 6: 'overig'}
         colorsDict = {0: "orange", 1: "yellow", 2: "green", 3: "", 4: "brown", 5: "grey", 6: "black"} # NEN-EN-ISO 14688-1 style, geen leem
@@ -1259,7 +1259,7 @@ class Bore(Test):
         plt.text(0.05, 0.2, 'Ingenieursbureau Gemeente Amsterdam Vakgroep Geotechniek Python ', fontsize=10)
 
 #        plt.tight_layout() # TODO: werkt niet met text die wrapt
-        plt.savefig(fname=f'{path}/{self.testid}.png')
+        plt.savefig(fname=f'{path}/{self.testid}.{outputType}')
         plt.close('all')
 
 
